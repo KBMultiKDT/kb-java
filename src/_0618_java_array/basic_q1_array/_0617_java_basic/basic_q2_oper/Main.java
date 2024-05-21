4a9e97396164e9991e31e0a2ae221a83ce5a5298
aliciawill <aliciawill@naver.com>
2024-05-21 12:12:28 +0900

    Java test

---
+++ src/_0618_java_array/basic_q1_array/_0617_java_basic/basic_q2_oper/Main.java
@@ -0,0 +1,11 @@
+package _0618_java_array.basic_q1_array._0617_java_basic.basic_q2_oper;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.printf("Hello and welcome!");
+
+        for (int i = 1; i <= 5; i++) {
+            System.out.println("i = " + i);
+        }
+    }
+}
