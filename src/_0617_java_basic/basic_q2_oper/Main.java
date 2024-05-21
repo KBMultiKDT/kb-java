5436f987be931efd148d8b157800133630324022
aliciawill <aliciawill@naver.com>
2024-05-21 12:45:06 +0900

    변수/연산자

--- src/_0617_java_basic/basic_q2_oper/Main.java
+++ src/_0617_java_basic/basic_q2_oper/Main.java
@@ -1,5 +1,7 @@
 package _0617_java_basic.basic_q2_oper;
 
+import javax.swing.*;
+
 public class Main {
     public static void main(String[] args) {
         System.out.printf("Hello and welcome!");
@@ -7,5 +9,9 @@ public class Main {
         for (int i = 1; i <= 5; i++) {
             System.out.println("i = " + i);
         }
+
+        JFrame f = new JFrame();
+        f.setSize(500, 500);
+        f.setVisible(true);
     }
 }
