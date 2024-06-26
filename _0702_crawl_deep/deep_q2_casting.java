e61c8e681b6cec274cd11ffc67280a862940f5f9
KBMultiKDT-강사용 <168627343+KBMultiKDT@users.noreply.github.com>
2024-06-26 14:33:45 +0900

    Add files via upload

---
+++ _0702_crawl_deep/deep_q2_casting.java
@@ -0,0 +1,31 @@
+package _0702_crawl_deep;
+
+abstract class Animal {
+    public abstract void speak();
+}
+
+class Dog extends Animal {
+    @Override
+    public void speak() {
+        System.out.println("Woof Woof!");
+    }
+}
+
+class Cat extends Animal {
+    @Override
+    public void speak() {
+        System.out.println("Meow Meow!");
+    }
+}
+
+public class deep_q2_casting {
+    public static void main(String[] args) {
+        Animal[] animals = new Animal[2];
+        animals[0] = new Dog();
+        animals[1] = new Cat();
+
+        for (Animal animal : animals) {
+            animal.speak();
+        }
+    }
+}
