5535cb4c940c04301b48262e19ed98ba5452a07a
KBMultiKDT-강사용 <168627343+KBMultiKDT@users.noreply.github.com>
2024-06-26 14:28:42 +0900

    Add files via upload

---
+++ _0702_crawl_basic/basic_q2_casting/FromFile.java
@@ -0,0 +1,24 @@
+package _0702_crawl_basic.basic_q2_casting;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class FromFile {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("검색하기 원하는 회사명 입력: ");
+        String fileName = sc.next();
+        fileName += ".txt";
+
+        try (BufferedReader br = new BufferedReader(new FileReader(fileName))) {
+            String line;
+            while ((line = br.readLine()) != null) {
+                System.out.println(line);
+            }
+        } catch (IOException e) {
+            System.out.println("파일을 읽는 중 오류가 발생했습니다: " + e.getMessage());
+        }
+    }
+}
