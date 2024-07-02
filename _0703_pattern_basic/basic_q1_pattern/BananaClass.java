235410ac066dc6422d0237c66acede31939f0a4e
KBMultiKDT-강사용 <168627343+KBMultiKDT@users.noreply.github.com>
2024-07-02 15:44:43 +0900

    Delete _0703_pattern_basic directory

--- _0703_pattern_basic/basic_q1_pattern/BananaClass.java
+++
@@ -1,7 +0,0 @@
-package _0703_pattern_basic.basic_q1_pattern;
-
-public class BananaClass {
-    public BananaClass() {
-        System.out.println("BananaClass 객체 생성자 호출되었습니다.");
-    }
-}
--- _0703_pattern_basic/basic_q1_pattern/IceClass.java
+++
@@ -1,7 +0,0 @@
-package _0703_pattern_basic.basic_q1_pattern;
-
-public class IceClass {
-    public IceClass() {
-        System.out.println("Ice 객체 생성자 호출되었습니다.");
-    }
-}
