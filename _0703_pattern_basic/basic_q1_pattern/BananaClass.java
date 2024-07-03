644673579db976d003ea59a3f2e86eb35737926e
KBMultiKDT-강사용 <168627343+KBMultiKDT@users.noreply.github.com>
2024-07-03 12:49:23 +0900

    Update BananaClass.java

--- _0703_pattern_basic/basic_q1_pattern/BananaClass.java
+++ _0703_pattern_basic/basic_q1_pattern/BananaClass.java
@@ -2,6 +2,6 @@ package _0703_pattern_basic.basic_q1_pattern;
 
 public class BananaClass {
     public BananaClass() {
-        System.out.println("BananaClass 객체 생성자 호출되었습니다.");
+        System.out.println("Banana 객체가 생성되었습니다.");
     }
 }
