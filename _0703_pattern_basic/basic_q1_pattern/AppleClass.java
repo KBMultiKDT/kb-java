2f28d2b8336ee86e81bdc159fb320c2d7a04c9c6
KBMultiKDT-강사용 <168627343+KBMultiKDT@users.noreply.github.com>
2024-07-03 12:49:00 +0900

    Update AppleClass.java

--- _0703_pattern_basic/basic_q1_pattern/AppleClass.java
+++ _0703_pattern_basic/basic_q1_pattern/AppleClass.java
@@ -2,6 +2,6 @@ package _0703_pattern_basic.basic_q1_pattern;
 
 public class AppleClass {
     public AppleClass() {
-        System.out.println("AppleClass 객체 생성자 호출되었습니다.");
+        System.out.println("Apple 객체가 생성되었습니다.");
     }
 }
